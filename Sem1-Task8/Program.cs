f8e8e9901cb1255e6f5464c7d93587f7bb469e44
Gusev <gse771@yandex.ru>
2022-10-14 23:18:17 +0300

    task8

---
+++ Sem1-Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8 семинар1
+Console.WriteLine("Введите любое положитеьное число");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+int begin = 1;
+if(namber1<=0)
+{
+    Console.Write("Недопустимое значенте");
+}
+else
+{
+    while(begin<=namber1) 
+    {
+        if(begin %2 ==0)  
+        {
+            Console.WriteLine(begin);
+            begin++;
+        }
+        else
+        {
+            begin++;
+        }
+
+    }
+} 
