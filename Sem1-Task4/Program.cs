80c5e39b5d98f17a9bcb4f92c965bff27d5b8a4c
Gusev <gse771@yandex.ru>
2022-10-14 22:18:45 +0300

    исправи опечатку

--- Sem1-Task4/Program.cs
+++ Sem1-Task4/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Введите первое число");
 int namber1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int namber2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.WriteLine("Введите третье число");
 int namber3 = Convert.ToInt32(Console.ReadLine());
 
 if(namber1>=namber2)
