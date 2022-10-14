6619ecd1d814547a1d1a40e190a8afc9b00f4be8
Gusev <gse771@yandex.ru>
2022-10-14 22:34:29 +0300

    Task4

---
+++ Sem1-Task6/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 6 семинар 1
+Console.WriteLine("Введите первое число");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int namber3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите четвертое число");
+int namber4 = Convert.ToInt32(Console.ReadLine());
+if(namber1 %2 == 0)
+{
+    Console.Write(namber1);
+    Console.WriteLine(" - четное число");
+}
+else
+{  
+     Console.Write(namber1);
+    Console.WriteLine(" - нечетное число");  
+}
+if(namber2 %2 ==0)
+{
+    Console.Write(namber2);
+    Console.WriteLine(" - четное число");
+}
+else
+{  
+     Console.Write(namber2);
+    Console.WriteLine(" - нечетное число");  
+}
+if(namber3 %2 ==0)
+{
+    Console.Write(namber3);
+    Console.WriteLine(" - четное число");
+}
+else
+{  
+     Console.Write(namber3);
+    Console.WriteLine(" - нечетное число");  
+}
+if(namber4% 2==0)
+{
+    Console.Write(namber4);
+    Console.WriteLine(" - четное число");
+}
+else
+{  
+     Console.Write(namber4);
+    Console.WriteLine(" - нечетное число");  
+}    
