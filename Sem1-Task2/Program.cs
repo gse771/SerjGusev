abaf78e99b1f397f7ae309efcd86217b1bbbb91f
Gusev <gse771@yandex.ru>
2022-10-14 21:30:09 +0300

    Task2

---
+++ Sem1-Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2 семинар1
+Console.WriteLine("Введите первое число");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+if(namber1>=namber2)
+{
+    Console.Write("max - ");
+    Console.WriteLine(namber1);
+}
+else
+{
+    Console.Write("max - ");
+    Console.WriteLine(namber2);  
+}     
+
+
+
