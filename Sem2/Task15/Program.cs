69a3eb643f3cfb2d9bb95b751b407283deef9ea4
Gusev <gse771@yandex.ru>
2022-10-20 10:14:28 +0300

    ДЗ 2 Задача 15

---
+++ Sem2/Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// задача 15: Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.WriteLine("Введите любое число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number>8)||(number<1))
+{
+    Console.WriteLine("Ошибка");
+}
+else
+{
+    if(number<6)
+    {
+        Console.WriteLine("Число  " + number  +  "  -  Невыходной день");
+    }
+    else
+    {
+        Console.WriteLine("Число  " + number  +  "  -   Выходной день");
+    }
+}    
