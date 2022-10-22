b20ab630ee1f03fa57422da643c7f9a2bde5c15c
Gusev <gse771@yandex.ru>
2022-10-22 12:12:36 +0300

    ДЗ 3 задача 19

---
+++ Sem3/Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿// На ввод пятизначное число. Проверить, является ли число полиндромом: да или нет.
+Console.WriteLine("Введите любое пятизначное число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<10000||num>99999)
+{
+    Console.WriteLine("Ошибка ввода числа");
+}
+else{
+    string num_str = num.ToString();
+    if(num_str[0]==num_str[4]&&num_str[1]== num_str[3])
+    {
+      Console.WriteLine("Да, это число является полиндромом");
+    }
+    else
+    {
+    Console.WriteLine("Нет, это число не является полиндромом");
+    }
+}
+    
