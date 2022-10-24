5b996d278aa20ab27d90c5833bfe7468d9fd4012
Gusev <gse771@yandex.ru>
2022-10-24 17:23:20 +0300

    ДЗ3 задача 23

---
+++ Sem3/Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Принимает на вход число (N) и выдает таблицу кубов чисел от1 до N
+int N = new Random().Next(1,20);
+Console.WriteLine("Выбрано число  "+N);
+double kub=1;
+int count=1;
+while(count<=N)
+{
+    kub=Math.Pow(count,3);
+    Console.Write(kub+"    ");
+    count=count+1;
+}
