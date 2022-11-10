df98c57037dc1c243763d309622171c1996fd039
Gusev <gse771@yandex.ru>
2022-11-10 19:22:51 +0300

    Task 26

---
+++ Sem4task26/Program.cs
@@ -0,0 +1,32 @@
+﻿// На вход число на выход сумма цифр в числе
+
+Console.WriteLine("Введите целое число ");
+int B = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+int Summa(int sum1)
+{
+    if(B>10)
+    {
+           
+        while(B>10)
+            {
+                sum1 += B%10;
+                //Console.WriteLine(sum1);
+                B /=10;
+                      
+            }
+            sum1 +=B;
+    }
+    else
+    {
+        
+        sum1 +=B;
+    }
+    
+return sum1;
+
+}
+Console.WriteLine(Summa(sum));
+
+
