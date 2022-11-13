11c6ec1a94983cb0342ab9a033c3022790c9ee66
Gusev <gse771@yandex.ru>
2022-11-13 21:13:15 +0300

    Семинар 5 задача 38

---
+++ Sem5task38/Program.cs
@@ -0,0 +1,35 @@
+﻿// Массив вещественные числа. Найти разницу между минимальным и максимальным элементами массива
+Console.WriteLine("Укажите нижнию границу массива");
+int A =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите верхнию границу массива");
+int B =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите размер массива");
+int L  =Convert.ToInt32(Console.ReadLine());
+double min =0;
+double max =0;
+double [] array =new double[L];
+for(int i =0; i<L; i++)
+{
+    array[i]=new Random().Next(A,B+1);
+    Console.Write(array[i] +" , ");
+    if (array[1] > min)
+    {
+//        min=min;
+    }
+    else
+    {
+        min =array[i];
+    }
+    if (array[i]>max)
+    {
+        max=array[i];
+    }
+    else
+    {
+ //       max=max;
+    }
+
+}
+Console.WriteLine();
+Console.WriteLine("Минимальное число =  " + min);
+Console.WriteLine("Максимальное число =  " + max );
