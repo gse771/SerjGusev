e23434c31481e616032ac55225a40b2ab7d34660
Gusev <gse771@yandex.ru>
2022-11-13 21:05:45 +0300

    Семинар 5 задача 34

---
+++ Sem5task34/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив, заполненный случайными трехзначными числами. Вывести сколько четных чисел в массиве
+Console.WriteLine("Укажите размер массива");
+int l= Convert.ToInt32(Console.ReadLine());
+int[] array = new int[l];
+int count = 0;
+for (int i =0; i<l;i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i] +" , ");
+    if (array[i]%2 ==0)
+    {
+        count +=1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных элементов в массиве = " + count);
