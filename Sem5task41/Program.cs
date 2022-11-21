b09b0d906f4334b77f53a68afc978e11b6a43547
Gusev <gse771@yandex.ru>
2022-11-21 12:46:31 +0300

    Задача 41 и 43

---
+++ Sem5task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь с клавиатуры вводит М чисел. Посчитать сколько положительных чисел ввел пользователь
+Console.Write("Задайте ряд любых чисел с клавиатуры, используя в качестве разделителя между вводимыми чисоами запятую  :  ");
+string text =Console.ReadLine();
+Console.WriteLine(text);
+Console.WriteLine();
+
+int StrLenght = text.Length;
+int i = 0;
+int Plus_ = 0; // Количество положительных чисел в строке
+string char_ = string.Empty;
+while (i < StrLenght)
+{
+    if (text[i] != ',')
+        {
+            char_ = char_ + text[i];
+        }
+    else
+        {
+//            Console.WriteLine(char_);
+             if (Convert.ToInt32(char_) > 0)
+                {
+                    Plus_ = Plus_ + 1;
+                }
+            char_ = string.Empty;   
+        }
+    i++;
+}
+//Console.WriteLine(char_);
+if (Convert.ToInt32(char_) > 0)
+    {
+        Plus_ = Plus_ + 1;
+    }
+Console.Write("Всео положительных чисел в строке: ");
+Console.WriteLine(Plus_);
---
+++ Sem5task43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Найти координаты точки пересечения двух прямых, заданных уравнениями: y=k1*x+b1; y=k2*x+b2. Значения задаются пользователем
+Console.WriteLine("Укажите коэфициент k1");
+double k1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите постоянную b1");
+double b1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите коэфициент k2, не равный k1");
+double k2 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите постоянную b2");
+double b2 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Уравнение для первой прямой: y="+k1+"*x+"+b1);
+Console.WriteLine("Уравнение для второй прямой: y="+k2+"*x+"+ b2);
+
+
+if(k1==k2)
+{
+    
+    if(b1==b2)
+    {
+        
+        Console.Write("Прямые совпадают");
+    }
+    else{
+        Console.Write("Прямые параллельны");
+    }
+}
+else
+{
+    double x =(b2-b1)/(k1-k2);
+
+    Console.WriteLine();
+    Console.WriteLine("Координаты точки пересечения:(" +x+";"+x+")");
+    Console.WriteLine();
+}    
+
+
+
